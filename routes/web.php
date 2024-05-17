75bb2da51c6e1d09083fe88f76e89f14b8565729
italloguedes <italo167@yahoo.com.br>
2024-05-16 22:24:06 -0300

    att `controllers`

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ChackoutController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+Route::get('checkout', ChackoutController::class);
